9da1502f25eaa275fa4c1d65580ee9bb44c23f90
Yll Gajtani <123652630+Yllgajtani@users.noreply.github.com>
2025-12-02 17:50:42 +0100

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+  <div className="continer">
+    <div className='JavaScirpt'>
+      <a href="" className='js'>Javascirpt</a>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam et, eos deserunt sint animi dolorem dicta suscipit temporibus autem sequi veniam repellendus eaque soluta totam modi officia incidunt velit laborum?</p>
+      <hr />
+    </div>
+    <div className="python">
+      <a href="" className='py'>Python</a>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam et, eos deserunt sint animi dolorem dicta suscipit temporibus autem sequi veniam repellendus eaque soluta totam modi officia incidunt velit laborum?</p>
+    <hr />
+    </div>
+    <div className="Html-css">
+      <a href="" className='ht'>Html-Css</a>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam et, eos deserunt sint animi dolorem dicta suscipit temporibus autem sequi veniam repellendus eaque soluta totam modi officia incidunt velit laborum?</p>
+    <hr />
+    </div>
+    <div className="backend">
+      <a href="" className='back'>Backend</a>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam et, eos deserunt sint animi dolorem dicta suscipit temporibus autem sequi veniam repellendus eaque soluta totam modi officia incidunt velit laborum?</p>
+    <hr />
+    </div>
+    <div className="Cc">
+      <a href="" className='C'>C#</a>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam et, eos deserunt sint animi dolorem dicta suscipit temporibus autem sequi veniam repellendus eaque soluta totam modi officia incidunt velit laborum?</p>
+    <hr />
+    </div>
+  </div>
+    </>
+  )
+}
+
+export default App
